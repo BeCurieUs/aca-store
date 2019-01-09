7fecbcebf0a8f91d47a2f3eb8384aff3a52cb62b
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-08 20:55:38 -0600

    Changes working list

--- public/index.js
+++ public/index.js
@@ -3,7 +3,10 @@
 const searchItems = () => {
   console.log(products)
 }
+const displayArray
+
 
 document.getElementById("itemlist").innerHTML = products.map((item,index)=>{
   return `<img src=${item.imgUrl} alt=${item.description} /><li>${item.name}</li>`
 }).join('')
+
