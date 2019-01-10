a4196d44f39563b376a8b265437124a5c0a1acd1
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-09 23:56:58 -0600

    default list and search functioning

--- public/index.js
+++ public/index.js
@@ -1,12 +1,39 @@
 "use strict";
 
-const searchItems = () => {
-  console.log(products)
+const imageCard = (item, index) => {
+  return `
+  <li class="product-item" >
+    <img src=${item.imgUrl} alt=${item.description} />
+    <p class="product-title">${item.name}</p>
+    <p class="product-rating">Rating: ${item.rating}</p>
+    <p class="product-reviews">Reviews: ${item.reviews.length}</p>
+    <p class="product-price">Price: ${item.price}</p>
+    <p class="product-description">description drop down placeholder</p>
+
+  </li>`
 }
-const displayArray = ""
 
+let displayArray = [];
 
-document.getElementById("itemlist").innerHTML = products.map((item,index)=>{
-  return `<img src=${item.imgUrl} alt=${item.description} /><li>${item.name}</li>`
-}).join('')
+if(displayArray){
+  products.forEach((item,index)=>{
+    displayArray.push(imageCard(item,index))
+  });
+}
+
+const searchItems = () => {
+  const formValue = document.getElementById("search-form").elements[0].value
+
+  displayArray = products.filter( (item,index) => {
+    return item.name.toLocaleLowerCase().includes(formValue) ||
+    item.description.toLocaleLowerCase().includes(formValue) ||
+    item.category.toLocaleLowerCase().includes(formValue);
+  }).map((fliteredItem,filteredIndex) => {
+    return imageCard(fliteredItem,filteredIndex)
+  })
+  document.getElementById("itemlist").innerHTML = displayArray
+
+}
+
+document.getElementById("itemlist").innerHTML = displayArray.join('');
 
